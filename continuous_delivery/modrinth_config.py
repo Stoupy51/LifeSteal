fb74fe787ab2747939f76a370255d7ee4bccc798
Stoupy51 <stoupy51@gmail.com>
2025-06-17 21:00:21 +0200

    refactor: ♻️ Update configuration loading to use stewbeet

--- continuous_delivery/modrinth_config.py
+++ continuous_delivery/modrinth_config.py
@@ -1,13 +1,26 @@
 
-# Configuration for Modrinth
-from config import *
+# Imports
+import os
+
+from beet import ProjectConfig, load_config, locate_config
+from stouputils.io import get_root_path
+
+# Try to find and load the beet configuration file
+cfg: ProjectConfig | None = None
+if config_path := locate_config(os.getcwd(), parents=True):
+	cfg = load_config(filename=config_path)
+	if cfg:
+		os.chdir(config_path.parent)
+if not cfg:
+	print(f"No beet config file found in the current directory '{os.getcwd()}'")
 
 # Constants
+ROOT: str = get_root_path(__file__, go_up=1)
 SUMMARY: str = "Life Steal FR is a simple datapack configurable aiming to add life steal behaviours from the original Lifesteal SMP:\nhttps://lifesteal.fandom.com/wiki/Season_1"
 
 DESCRIPTION_MARKDOWN: str = ""
 if os.path.exists(f"{ROOT}/README.md"):
-	with open(f"{ROOT}/README.md", "r", encoding="utf-8") as file:
+	with open(f"{ROOT}/README.md", encoding="utf-8") as file:
 		DESCRIPTION_MARKDOWN = file.read()
 else:
 	print("README.md not found, description_markdown will be empty")
@@ -21,12 +34,12 @@ VERSION_TYPE: str = "release"
 # Configuration
 modrinth_config: dict = {
 	"slug": "lifestealfr",
-	"project_name": PROJECT_NAME,
-	"version": VERSION,
+	"project_name": cfg.name,
+	"version": cfg.version,
 	"summary": SUMMARY,
 	"description_markdown": DESCRIPTION_MARKDOWN,
 	"dependencies": DEPENDENCIES,
 	"version_type": VERSION_TYPE,
-	"build_folder": BUILD_FOLDER,
+	"build_folder": cfg.output,
 }
 
