4f8ba6722b0153ad1871bf52179abdd184e378e0
Stoupy51 <stoupy51@gmail.com>
2025-12-02 21:26:08 +0100

    feat: ✨ Enhance configuration display with gradient text and interactive elements

--- user/link.py
+++ user/link.py
@@ -1,8 +1,10 @@
 
 # ruff: noqa: E501
 # Imports
+import json
+
 from beet import Advancement, Context, LootTable
-from stewbeet import JsonDict, set_json_encoder, write_function, write_load_file, write_tick_file
+from stewbeet import JsonDict, create_gradient_text, set_json_encoder, write_function, write_load_file, write_tick_file
 
 
 # Main function is run just before making finalyzing the build process (zip, headers, lang, ...)
@@ -339,25 +341,27 @@ execute as @a at @s run playsound entity.experience_orb.pickup ambient @s
 """)
 
 	# Interactive configuration display function
+	LIFE_STEAL_TEXT: str = json.dumps(create_gradient_text("[Life Steal Configuration]", "#FF0000", "#FF7300"))
+	NUMERIC_SETTING: str = json.dumps(create_gradient_text("Numeric Settings:", "#68D4D4", "#009696"))
+	BOOLEAN_SETTING: str = json.dumps(create_gradient_text("Toggle Settings (1 = enabled, 0 = disabled):", "#68D4D4", "#009696"))
 	write_function(f"{ns}:_config", f"""
 # Display configuration header
-tellraw @s {{"text":"[Life Steal Configuration]","color":"gold"}}
+tellraw @s {LIFE_STEAL_TEXT}
 
 # Numeric settings
-tellraw @s {{"text":"\\nNumeric Settings:","color":"aqua"}}
-tellraw @s [{{"text":"- Max Hearts: ","color":"aqua"}},{{"score":{{"name":"MAX_HEARTS","objective":"{ns}.data"}},"color":"yellow"}},{{"text":" [click]","color":"gray","click_event":{{"action":"suggest_command","command":"/scoreboard players set MAX_HEARTS {ns}.data 20"}},"hover_event":{{"action":"show_text","value":{{"text":"Enter the maximum number of hearts a player can have\\nDefault: 20","color":"gray"}}}}}}]
-tellraw @s [{{"text":"- Min Hearts: ","color":"aqua"}},{{"score":{{"name":"MIN_HEARTS","objective":"{ns}.data"}},"color":"yellow"}},{{"text":" [click]","color":"gray","click_event":{{"action":"suggest_command","command":"/scoreboard players set MIN_HEARTS {ns}.data 1"}},"hover_event":{{"action":"show_text","value":{{"text":"Enter the minimum number of hearts a player can have\\nDefault: 1","color":"gray"}}}}}}]
-tellraw @s [{{"text":"- Revived Hearts: ","color":"aqua"}},{{"score":{{"name":"REVIVED_HEARTS","objective":"{ns}.data"}},"color":"yellow"}},{{"text":" [click]","color":"gray","click_event":{{"action":"suggest_command","command":"/scoreboard players set REVIVED_HEARTS {ns}.data 4"}},"hover_event":{{"action":"show_text","value":{{"text":"Enter the number of hearts a player respawns with when revived\\nDefault: 4","color":"gray"}}}}}}]
+tellraw @s ["\\n",{NUMERIC_SETTING}]
+tellraw @s [{{"text":"- Max Hearts: ","color":"aqua","click_event":{{"action":"suggest_command","command":"/scoreboard players set MAX_HEARTS {ns}.data 20"}},"hover_event":{{"action":"show_text","value":{{"text":"Enter the maximum number of hearts a player can have\\nDefault: 20","color":"white"}}}}}},{{"score":{{"name":"MAX_HEARTS","objective":"{ns}.data"}},"color":"yellow"}},{{"text":" 👈","color":"gray"}}]
+tellraw @s [{{"text":"- Min Hearts: ","color":"aqua","click_event":{{"action":"suggest_command","command":"/scoreboard players set MIN_HEARTS {ns}.data 1"}},"hover_event":{{"action":"show_text","value":{{"text":"Enter the minimum number of hearts a player can have\\nDefault: 1","color":"white"}}}}}},{{"score":{{"name":"MIN_HEARTS","objective":"{ns}.data"}},"color":"yellow"}},{{"text":" 👈","color":"gray"}}]
+tellraw @s [{{"text":"- Revived Hearts: ","color":"aqua","click_event":{{"action":"suggest_command","command":"/scoreboard players set REVIVED_HEARTS {ns}.data 4"}},"hover_event":{{"action":"show_text","value":{{"text":"Enter the number of hearts a player respawns with when revived\\nDefault: 4","color":"white"}}}}}},{{"score":{{"name":"REVIVED_HEARTS","objective":"{ns}.data"}},"color":"yellow"}},{{"text":" 👈","color":"gray"}}]
 
 # Boolean settings
-tellraw @s {{"text":"\\nToggle Settings (1 = enabled, 0 = disabled):","color":"aqua"}}
-execute if score NATURAL_DEATH_HEART_DROP {ns}.data matches 1 run tellraw @s [{{"text":"- Natural Death Heart Drop: ","color":"aqua"}},{{"text":"Enabled","color":"green"}},{{"text":" [click]","color":"gray","click_event":{{"action":"suggest_command","command":"/scoreboard players set NATURAL_DEATH_HEART_DROP {ns}.data 0"}},"hover_event":{{"action":"show_text","value":{{"text":"Click to disable - Players won't drop hearts when dying to non-player causes\\nDefault: Enabled","color":"gray"}}}}}}]
-execute if score NATURAL_DEATH_HEART_DROP {ns}.data matches 0 run tellraw @s [{{"text":"- Natural Death Heart Drop: ","color":"aqua"}},{{"text":"Disabled","color":"red"}},{{"text":" [click]","color":"gray","click_event":{{"action":"suggest_command","command":"/scoreboard players set NATURAL_DEATH_HEART_DROP {ns}.data 1"}},"hover_event":{{"action":"show_text","value":{{"text":"Click to enable - Players will drop hearts when dying to non-player causes\\nDefault: Enabled","color":"gray"}}}}}}]
-execute if score USE_HALF_HEARTS {ns}.data matches 1 run tellraw @s [{{"text":"- Half Hearts Mode: ","color":"aqua"}},{{"text":"Enabled","color":"green"}},{{"text":" [click]","color":"gray","click_event":{{"action":"suggest_command","command":"/scoreboard players set USE_HALF_HEARTS {ns}.data 0"}},"hover_event":{{"action":"show_text","value":{{"text":"Click to disable - Hearts will be tracked in whole numbers\\nWarning: This will convert all players' hearts!\\nDefault: Disabled","color":"gray"}}}}}}]
-execute if score USE_HALF_HEARTS {ns}.data matches 0 run tellraw @s [{{"text":"- Half Hearts Mode: ","color":"aqua"}},{{"text":"Disabled","color":"red"}},{{"text":" [click]","color":"gray","click_event":{{"action":"suggest_command","command":"/scoreboard players set USE_HALF_HEARTS {ns}.data 1"}},"hover_event":{{"action":"show_text","value":{{"text":"Click to enable - Hearts will be tracked in 0.5 increments\\nWarning: This will convert all players' hearts!\\nDefault: Disabled","color":"gray"}}}}}}]
-execute if score BAN_BELOW_MIN_HEARTS {ns}.data matches 1 run tellraw @s [{{"text":"- Ban Below Min Hearts: ","color":"aqua"}},{{"text":"Enabled","color":"green"}},{{"text":" [click]","color":"gray","click_event":{{"action":"suggest_command","command":"/scoreboard players set BAN_BELOW_MIN_HEARTS {ns}.data 0"}},"hover_event":{{"action":"show_text","value":{{"text":"Click to disable - Players won't be banned when reaching minimum hearts\\nDefault: Enabled","color":"gray"}}}}}}]
-execute if score BAN_BELOW_MIN_HEARTS {ns}.data matches 0 run tellraw @s [{{"text":"- Ban Below Min Hearts: ","color":"aqua"}},{{"text":"Disabled","color":"red"}},{{"text":" [click]","color":"gray","click_event":{{"action":"suggest_command","command":"/scoreboard players set BAN_BELOW_MIN_HEARTS {ns}.data 1"}},"hover_event":{{"action":"show_text","value":{{"text":"Click to enable - Players will be banned when reaching minimum hearts\\nDefault: Enabled","color":"gray"}}}}}}]
+tellraw @s ["\\n",{BOOLEAN_SETTING}]
+execute if score NATURAL_DEATH_HEART_DROP {ns}.data matches 1 run tellraw @s [{{"text":"- Natural Death Heart Drop: ","color":"aqua","click_event":{{"action":"suggest_command","command":"/scoreboard players set NATURAL_DEATH_HEART_DROP {ns}.data 0"}},"hover_event":{{"action":"show_text","value":{{"text":"Click to disable - Players won't drop hearts when dying to non-player causes\\nDefault: Enabled","color":"white"}}}}}},{{"text":"Enabled","color":"green"}},{{"text":" 👈","color":"gray"}}]
+execute if score NATURAL_DEATH_HEART_DROP {ns}.data matches 0 run tellraw @s [{{"text":"- Natural Death Heart Drop: ","color":"aqua","click_event":{{"action":"suggest_command","command":"/scoreboard players set NATURAL_DEATH_HEART_DROP {ns}.data 1"}},"hover_event":{{"action":"show_text","value":{{"text":"Click to enable - Players will drop hearts when dying to non-player causes\\nDefault: Enabled","color":"white"}}}}}},{{"text":"Disabled","color":"red"}},{{"text":" 👈","color":"gray"}}]
+execute if score USE_HALF_HEARTS {ns}.data matches 1 run tellraw @s [{{"text":"- Half Hearts Mode: ","color":"aqua","click_event":{{"action":"suggest_command","command":"/scoreboard players set USE_HALF_HEARTS {ns}.data 0"}},"hover_event":{{"action":"show_text","value":{{"text":"Click to disable - Hearts will be tracked in whole numbers\\nWarning: This will convert all players' hearts!\\nDefault: Disabled","color":"white"}}}}}},{{"text":"Enabled","color":"green"}},{{"text":" 👈","color":"gray"}}]
+execute if score USE_HALF_HEARTS {ns}.data matches 0 run tellraw @s [{{"text":"- Half Hearts Mode: ","color":"aqua","click_event":{{"action":"suggest_command","command":"/scoreboard players set USE_HALF_HEARTS {ns}.data 1"}},"hover_event":{{"action":"show_text","value":{{"text":"Click to enable - Hearts will be tracked in 0.5 increments\\nWarning: This will convert all players' hearts!\\nDefault: Disabled","color":"white"}}}}}},{{"text":"Disabled","color":"red"}},{{"text":" 👈","color":"gray"}}]
+execute if score BAN_BELOW_MIN_HEARTS {ns}.data matches 1 run tellraw @s [{{"text":"- Ban Below Min Hearts: ","color":"aqua","click_event":{{"action":"suggest_command","command":"/scoreboard players set BAN_BELOW_MIN_HEARTS {ns}.data 0"}},"hover_event":{{"action":"show_text","value":{{"text":"Click to disable - Players won't be banned when reaching minimum hearts\\nDefault: Enabled","color":"white"}}}}}},{{"text":"Enabled","color":"green"}},{{"text":" 👈","color":"gray"}}]
+execute if score BAN_BELOW_MIN_HEARTS {ns}.data matches 0 run tellraw @s [{{"text":"- Ban Below Min Hearts: ","color":"aqua","click_event":{{"action":"suggest_command","command":"/scoreboard players set BAN_BELOW_MIN_HEARTS {ns}.data 1"}},"hover_event":{{"action":"show_text","value":{{"text":"Click to enable - Players will be banned when reaching minimum hearts\\nDefault: Enabled","color":"white"}}}}}},{{"text":"Disabled","color":"red"}},{{"text":" 👈","color":"gray"}}]
 """)
-
 	pass
 
